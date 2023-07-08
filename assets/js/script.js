63e188b9fd1e06b8aade0fbcc893cc4e116e3595
Siesta <skyguard2121@gmail.com>
2023-07-08 14:40:12 +0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-/* ============= TYPİNG ANIMATION ============= */
-var typed6 = new Typed('.types', {
-    strings: ['Turkish', 'Photographer', 'Video Editor', 'Designer', 'Developer', 'Coder'],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
